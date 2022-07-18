9d601a3087be6721a62fdb22a45379186e4a1f33
prateekupadhyay1997 <prateekupadhyay1997@gmail.com>
2022-07-18 15:28:15 +0530

    first commit

---
+++ MLOps_DVC_MLFlow/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLOps on sample dataset with DVC and MLFlow',
+    author='prateek upadhyay',
+    license='MIT',
+)
